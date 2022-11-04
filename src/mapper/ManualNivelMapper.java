171fe92ae19f597ec3a06c25f266ff2e77d06e67
David <davidcq55@gmail.com>
2022-11-03 20:57:17 -0500

    Se agrega la capa mapper al proyecto

--- src/mapper/ManualNivelMapper.java
+++ src/mapper/ManualNivelMapper.java
@@ -1,2 +1,22 @@
-package mapper;public class ManualNivelMapper {
+package mapper;
+
+import entidades.Nivel;
+import transferobject.NivelDTO;
+import transferobject.PreguntaDTO;
+
+import java.util.ArrayList;
+
+public class ManualNivelMapper {
+
+    public NivelDTO CreateDTO(Nivel entidad){
+
+        NivelDTO result = new NivelDTO();
+
+            result.setCategoria(entidad.getCategoria());
+            result.setDificultad(entidad.getDificultad());
+            result.setPuntos(entidad.getPuntos());
+            result.setPreguntas(new ArrayList<PreguntaDTO>());
+
+            return result;
+    }
 }
